338240f95801c374ad029b446791159b69df990c
Andre Marques <dev.andrem166@gmail.com>
2023-02-22 17:32:21 -0300

    Firts Steps in Java With Spring Boot

---
+++ 05_FirstStepsInJavaWithSpring/rest-with-spring-boot-end-java/src/main/java/br/com/andre/restwithspringbootendjava/Startup.java
@@ -0,0 +1,13 @@
+package br.com.andre.restwithspringbootendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ 05_FirstStepsInJavaWithSpring/rest-with-spring-boot-end-java/src/test/java/br/com/andre/restwithspringbootendjava/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.andre.restwithspringbootendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
